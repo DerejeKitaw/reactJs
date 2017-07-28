62f0b04710fc00b0cf8d55a65fc1f843410be767
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-07-28 19:56:05 -0400

    set directory

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack =require("webpack");
+var path= require("path");
+
+// Setup distribuition directory
+var DIST_DIR = path.resolve(__dirname, "dist");
+// Setup source directory
+var SRS_DIR = path.resolve(__dirname, "src");
+
+
