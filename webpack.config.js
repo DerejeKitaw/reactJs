f837b916b2e214cf2b4ed173bba01273df5cc962
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-07-28 20:01:15 -0400

    webpack config defined

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,25 @@ var path= require("path");
 // Setup distribuition directory
 var DIST_DIR = path.resolve(__dirname, "dist");
 // Setup source directory
-var SRS_DIR = path.resolve(__dirname, "src");
+var SRC_DIR = path.resolve(__dirname, "src");
 
-
+var config ={
+    entry: SRC_DIR + "/app/index.js",
+    output:{
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    modules:{
+        loaders:[
+            {
+                test: /\.js?/,
+                include:SRC_DIR,
+                loader:"babel-loader",
+                query: {
+                    preset: ["react", "es2015","stage-2"]
+                }
+            }
+        ]
+    }
+}
