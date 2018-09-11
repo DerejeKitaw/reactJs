1399c6b9c5b8b7cfcaf59d326c348a36c901cabf
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-11 00:28:01 -0400

    simple route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import  Route from './components/Route';
+import Pacific from './components/Pacific';
+import Atlantic from './components/Atlantic';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App works
+      <Route path='/atlantic' component={Atlantic} />
+      <Route path='/pacific' component={Pacific} />
       </div>
     );
   }
