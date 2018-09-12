50a19ce0a5a781b123086d84359eb51d6376e07b
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-11 22:40:01 -0400

    Using react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import Route from './components/Route';
-import Link from './components/Link';
-import Router from './components/Router';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 import Pacific from './components/Pacific';
 import Atlantic from './components/Atlantic';
 import BlackSea from './components/BlackSea';
 
-import createHistory from 'history/createBrowserHistory';
-const history = createHistory();
-
 class App extends Component {
-  componentDidMount() {
-    history.listen(() => this.forceUpdate());
-  }
+  
   render() {
     return (
       <Router>
