7cc6fc91c29a7a02a60249e61c342c8f39939dc1
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-22 00:41:13 -0400

    empty component with scss

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import Route from './components/Route';
-import Link from './components/Link';
-import Pacific from './components/Pacific';
-import Atlantic from './components/Atlantic';
-
-import createHistory from 'history/createBrowserHistory';
-const history = createHistory();
-
+import './index.css';
+import Home from './components/Home'
 class App extends Component {
-  componentDidMount() {
-    history.listen(() => this.forceUpdate());
-  }
+
   render() {
     return (
-      <div className="App">
-        <ul>
-          {' '}
-          <li>
-            <Link to="/atlantic">
-              <code>/atlantic</code>
-            </Link>
-          </li>{' '}
-          <li>
-            <Link to="/pacific">
-              <code>/pacific</code>
-            </Link>
-          </li>{' '}
-        </ul>
-        <hr />
-        <Route path="/atlantic" component={Atlantic} />
-        <Route path="/pacific" component={Pacific} />
+      <div>
+        <Home />
       </div>
     );
   }
